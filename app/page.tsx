244fa3ae65bd0975c22495a879104fd8acd1736e
supanat00 <supanat90@gmail.com>
2024-07-11 12:59:25 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { SnapButton } from "@/components/SnapButton";
+
+export default function App() {
+
+  return (
+    <main className="App">
+      <SnapButton />
+    </main>
+  );
+}
